8002ff5845f006d6968635d806c6d25cc6bd3743
jingch <b06209027@ntu.edu.tw>
2019-11-23 23:50:12 +0800

    add temp code of training process

--- task1/src/analysis/plot.py
+++ task1/src/analysis/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import json
 
 
 def plot_box(data, y_ticks=False, save_path=None):
@@ -24,3 +25,29 @@ def plot_box(data, y_ticks=False, save_path=None):
 
 def plot_confusion_matrix(y_true, y_pred):
     return
+
+
+def plot_history(history_path):
+    with open(history_path, 'r') as f:
+        history = json.loads(f.read())
+
+    train_loss = [l['loss'] for l in history['train']]
+    valid_loss = [l['loss'] for l in history['valid']]
+    train_f1 = [l['f1'] for l in history['train']]
+    valid_f1 = [l['f1'] for l in history['valid']]
+
+    plt.figure(figsize=(7, 5))
+    plt.title('Loss')
+    plt.plot(train_loss, label='train')
+    plt.plot(valid_loss, label='valid')
+    plt.legend()
+    plt.show()
+
+    plt.figure(figsize=(7, 5))
+    plt.title('F1 Score')
+    plt.plot(train_f1, label='train')
+    plt.plot(valid_f1, label='valid')
+    plt.legend()
+    plt.show()
+
+    print('Best F1 score ', max([[l['f1'], idx] for idx, l in enumerate(history['valid'])]))
--- task1/src/preprocessor.py
+++ task1/src/preprocessor.py
@@ -1,4 +1,4 @@
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 
 
 def label_to_onehot(labels):
